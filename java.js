6cbb7acc2b637864f36386c6bdeb99018fa2cbe1
MateoDelEon670 <109543311+MateoDelEon670@users.noreply.github.com>
2022-09-03 09:57:08 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,39 @@
+canvas =document.getElementById("canvas");
+
+ctx =canvas.getContext("2d");
+color ="blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+
+canvas.addEventListener("mousedown",mouse);
+
+function mouse(e)
+{
+    color = document.getElementById("color").value;
+    console.log(color);
+    //final de la actividad adicional
+    
+     mouse_x = e.clientX - canvas.offsetLeft;
+     mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
+    circle(mouse_x , mouse_y);    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
